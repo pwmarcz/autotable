d23f65633cba8cc802358b9d0dca9c6609f962f9
Paweł Marczewski <pwmarcz@gmail.com>
2025-08-22 21:54:40 +0200

    Add three to global namespace

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import 'bootstrap/dist/js/bootstrap';
 import { AssetLoader } from './asset-loader';
 import { Game } from './game';
+import * as three from 'three';
 
 const assetLoader = new AssetLoader();
 
@@ -9,6 +10,6 @@ const assetLoader = new AssetLoader();
 assetLoader.loadAll().then(() => {
   const game = new Game(assetLoader);
   // for debugging
-  Object.assign(window, {game});
+  Object.assign(window, {game, three});
   game.start();
 });
